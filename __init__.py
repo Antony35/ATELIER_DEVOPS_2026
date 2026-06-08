d52b49b83360f8e1b0a6e730349e2cb9410a1435
Antony Huart <107783882+Antony35@users.noreply.github.com>
2026-06-08 09:05:15 +0000

    Conteneurise l'app Flask (Dockerfile, deps épinglées, écoute 0.0.0.0)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from flask import Flask
 from flask import render_template
-from flask import json
-import sqlite3
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
@@ -14,4 +12,4 @@ def exercices():
     return render_template('exercices.html')
                                                                                                                
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
